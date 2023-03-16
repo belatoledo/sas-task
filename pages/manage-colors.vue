9ba2c111585e1e2af61c90e5556846d76dba467b
belatoledo <it.amancio@gmail.com>
2023-03-16 18:10:50 -0300

    feat: dashboard and manage pages layout

---
+++ pages/manage-colors.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      Here you manage your colors.
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'ManagerPage'
+}
+</script>

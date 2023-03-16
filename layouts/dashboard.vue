e72586557f4a2d3a008474074103d4a1715ba4d3
belatoledo <it.amancio@gmail.com>
2023-03-16 15:09:17 -0300

    feat: home page and layout dashboard

---
+++ layouts/dashboard.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-app>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+
+      <v-toolbar-title class="header-logo flexbox">
+        <img width="30" src="../assets/images/rgb-bg.png" alt="RGB Colors">
+        <span class="ml-3 font-weight-medium">Pattern Colors</span>
+      </v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <nuxt-link to="/login" @click="logout()">
+        <v-icon>mdi-logout</v-icon>
+        Logout
+      </nuxt-link>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>Made by Izabela Toledo • &copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+// import HeaderComponent from '../components/HeaderComponent.vue'
+
+export default {
+  name: 'DashboardLayout',
+  components: {
+    // HeaderComponent
+  },
+  data () {
+    return {
+      clipped: true,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'My collection',
+          to: '/collection'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Manage colors',
+          to: '/manage-colors'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,46 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-app>
+    <HeaderComponent />
+    <v-main class="bg-effect">
+      <img src="../assets/images/home-bg.svg" :width="$vuetify.breakpoint.smAndDown ? 300 : 600" class="mt-16 pt-16 flexbox">
+      <v-btn
+        class="mt-12 flexbox btn-rounded"
+        width="300px"
+        height="68"
+        :style="$vuetify.breakpoint.smAndDown ? 'font-size: 18px !important' : 'font-size: 28px !important;'"
+        to="/login"
+      >
+        Login
+      </v-btn>
+      <h1 class="mt-8 white--text text-uppercase text-center font-weight-bold" :class="$vuetify.breakpoint.smAndDown ? 'title-text-mobile' : 'title-text'">
+        and manage your <span class="primary--text">colors</span>
+      </h1>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+import HeaderComponent from '@/components/HeaderComponent.vue'
+
 export default {
-  name: 'IndexPage'
+  name: 'HomePage',
+  components: {
+    HeaderComponent
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.bg-effect {
+  min-height: 100vh !important;
+  background: rgb(188,250,251);
+  background: linear-gradient(90deg, rgba(188,250,251,1) 0%, rgba(217,193,255,1) 21%, rgba(255,221,135,1) 42%, rgba(253,210,189,1) 58%, rgba(251,199,246,1) 75%, rgba(228,255,166,1) 100%);
+  animation: colors 8s infinite alternate ease-in;
+}
+
+@keyframes colors {
+  50% {
+    filter: hue-rotate(360deg)  ;
+  }
+}
+</style>
